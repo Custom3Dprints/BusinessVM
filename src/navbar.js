2e9a62740410a8852d54050d5fa4100578a4a863
Custom3Dprints <marjorie.3dprints@gmail.com>
2024-10-27 21:01:06 -0400

    Events done

---
+++ src/navbar.js
@@ -0,0 +1,19 @@
+// Fetch navbar.html and insert its content into #navbar
+fetch('../public/navbar.html')
+	.then(response => response.text())
+	.then(html => {
+		document.getElementById('navbar').innerHTML = html;
+		
+		// Attach event listeners
+		document.getElementById('Events-link').addEventListener('click', () => window.location.href = "../public/Events.html");
+		document.getElementById('Reservations-link').addEventListener('click', () => window.location.href = "../public/Reservations.html");
+		
+		
+		// Mobile menu toggle
+		const menu = document.querySelector('#mobile-menu');
+		const menuLinks = document.querySelector('.navbar__menu');
+		menu.addEventListener('click', function(){
+			menu.classList.toggle('is-active');
+			menuLinks.classList.toggle('active');
+		});
+	});
